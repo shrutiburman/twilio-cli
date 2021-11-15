d5bc9c0ecc6775f77c7e112ca99ccb812371d4eb
shrutiburman <87537688+shrutiburman@users.noreply.github.com>
2021-11-15 16:29:09 +0530

    181 testing cli (#2)
    * Initial commit: added draft release step and delete draft release

---
+++ .github/scripts/revert-commit-draft-release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Running revert last commit script"
+prevCommit=$(git rev-parse --verify HEAD)
+echo prevCommit
+cd "${GITHUB_WORKSPACE}"
+echo "Dummy change to see revert ++"
+git branch ${GITHUB_REF}
+echo "Git configurations"
+git config --global user.email "team_interfaces+github@twilio.com"
+git config --global user.name "twilio-dx"
+git reset --hard HEAD~1
+git push --force
